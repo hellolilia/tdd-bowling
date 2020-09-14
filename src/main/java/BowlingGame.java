cf196851cf75807bfc6ceece6634c83e33e9baff
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-09-14 22:24:57 +0800

    implement return 300 when all frames are strike

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,7 +1,21 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class BowlingGame {
 
 
-    public int calculate(int[] ints) {
-        return 0;
+    public int calculate(String scoring) {
+
+        List<Integer> scores = Stream.of(scoring.split(","))
+                .filter(n -> !n.equals("X"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        int allPoints = 0;
+        for (int i = 0; i < scores.size()-2; i++){
+            allPoints += scores.get(i) + scores.get(i+1) + scores.get(i+2);
+        }
+
+        return allPoints;
     }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -5,10 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BowlingGameTest {
     @Test
     void should_return_0_when_all_frames_are_0_points(){
-        BowlingGame bowlingpoints = new BowlingGame();
-        int result = bowlingpoints.calculate(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
+        BowlingGame bowlingScore = new BowlingGame();
+        int result = bowlingScore.calculate("0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0");
         assertEquals(0,result);
+    }
 
-
+    @Test
+    void should_return_300_when_all_frames_are_strike(){
+        BowlingGame bowlingScore = new BowlingGame();
+        int result = bowlingScore.calculate("10,X,10,X,10,X,10,X,10,X,10,X,10,X,10,X,10,X,10,10,10");
+        assertEquals(300,result);
     }
 }
