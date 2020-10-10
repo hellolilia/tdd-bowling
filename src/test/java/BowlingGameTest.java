46646ca6f1375acfcd4e8aed2b4a9f6049d95aa7
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-10-10 23:36:09 +0800

    code refactoring

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTest {
+
     @Test
     void should_return_0_when_all_frames_are_0_points(){
         BowlingGame bowlingScore = new BowlingGame();
@@ -18,7 +19,7 @@ public class BowlingGameTest {
     }
 
     @Test
-    void should_return_right_bowling_scoring(){
+    void should_return_right_bowling_scoring_when_last_frame_is_strike(){
         BowlingGame bowlingScore = new BowlingGame();
         int result = bowlingScore.calculate("9,/,X, ,8,1,X, ,X, ,7,/,5,4,X, ,X, ,X,8,0");
         assertEquals(195,result);
