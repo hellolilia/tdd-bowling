ce2ede99c1362b9c953c81ab47b239a6325cecac
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-10-10 23:39:35 +0800

    add test should return right bowling scoring when last frame is not spare or strike

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -31,4 +31,11 @@ public class BowlingGameTest {
         int result = bowlingScore.calculate("7,0,8,/,9,/,X, ,8,0,X, ,X, ,X, ,8,/,9,/,8");
         assertEquals(187,result);
     }
+
+    @Test
+    void should_return_right_bowling_scoring_when_last_frame_is_not_spare_or_strike(){
+        BowlingGame bowlingScore = new BowlingGame();
+        int result = bowlingScore.calculate("3,/,7,0,5,2,5,3,1,5,7,0,8,0,9,0,6,0,6,1,0");
+        assertEquals(82,result);
+    }
 }
